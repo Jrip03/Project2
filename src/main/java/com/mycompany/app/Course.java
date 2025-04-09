c5a70a9f8a8d5f6268ae276277d3aef3de7ab3cd
Jrip03 <126905535+Jrip03@users.noreply.github.com>
2025-04-09 13:07:56 -0500

    Added Course

---
+++ src/main/java/com/mycompany/app/Course.java
@@ -0,0 +1,43 @@
+public class Course{
+    private final String courseName;
+    private final String courseID;
+    private int classSize;
+    private ArrayList<Student> roster;
+    private final Teacher teacher;
+
+    public Course(String name, String id, ArrayList<Student> studentList, Teacher teacher){
+        courseName = name;
+        courseID = id;
+        roster = studentList;
+        classSize = roster.length();
+        this.teacher = teacher;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public String getCourseID(){
+        return courseID;
+    }
+
+    public int getClassSize(){
+        return classSize;
+    }
+
+    public Student[] getStudentList(){
+        return roster;
+    }
+
+    public Teacher getTeacher(){
+        return teacher;
+    }
+
+    public void addStudent(Student student){
+        roster.append(student);
+    }
+
+    public void removeStudent(String studentName){
+        
+    }
+}
