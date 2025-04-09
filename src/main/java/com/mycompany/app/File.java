865c73f4f6477dde59ca16b5db2070c6b9cbb5f0
mattholliday123 <mattholliday430@sbcglobal.net>
2025-04-09 13:03:04 -0500

    File class

---
+++ src/main/java/com/mycompany/app/File.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+import java.io.*;
+
+public class File {
+    
+
+
+    public void readFile(String filename){
+
+    }
+
+    public void writeFile(){
+
+    }
+
+    public void retrieveFile(Course course){
+
+    }
+
+    public void storeFile(){
+
+    }
+}
