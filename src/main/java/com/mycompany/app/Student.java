d2314afe1c2418796f56067c88e31b5b5de2043c
Ta0845 <AMBALATHANVI123@GMAIL.COM>
2025-04-16 10:19:55 -0500

    Student class

--- src/main/java/com/mycompany/app/Student.java
+++ src/main/java/com/mycompany/app/Student.java
@@ -1,5 +1,38 @@
 package com.mycompany.app;
 
 public class Student {
+   private String studentID;
+   private String firstName;
+   private String lastName;
    
+   public Student(String fName, String lName, String ID) {
+     studentID = ID;
+     firstName = fName;
+     lastName = lName;
+   }
+
+   public String getStudentID() {
+    return studentID;
+   }
+
+   public void setStudentID(String studentID) {
+    this.studentID = studentID;
+   }
+
+   public String getFirstName() {
+    return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+    this.firstName = firstName;
+   }
+
+   public String getLastName() {
+    return lastName;
+   }
+
+   public void setLastName(String lastName) {
+    this.lastName = lastName;
+   }
+
 }
--- src/main/java/com/mycompany/app/Teacher.java
+++ src/main/java/com/mycompany/app/Teacher.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
 public class Teacher {
-   
+    private String firstName;
+    private String lastName;
+
+    public Teacher(String fName, String lName){
+     firstName = fName;
+     lastName = lName;
+    }
 }
