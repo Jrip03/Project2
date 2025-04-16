f64d33f97aace093e745c0ff49b392d465e51189
Ta0845 <AMBALATHANVI123@GMAIL.COM>
2025-04-16 12:41:07 -0500

    Changes to Teacher

--- src/main/java/com/mycompany/app/Teacher.java
+++ src/main/java/com/mycompany/app/Teacher.java
@@ -3,9 +3,29 @@ package com.mycompany.app;
 public class Teacher {
     private String firstName;
     private String lastName;
+    private ArrayList<Course> courseList;
 
     public Teacher(String fName, String lName){
      firstName = fName;
      lastName = lName;
     }
+
+    public String getFirstName() {
+        return firstName;
+       }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+       }
+    
+    public String getLastName() {
+        return lastName;
+       }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+       }
+
+    
+    
 }
