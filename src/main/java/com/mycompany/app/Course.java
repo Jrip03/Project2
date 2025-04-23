3b087e4d6de82a1f6a7f3d8499f2b49c020b2a8d
Jrip03 <jrip03lz@gmail.com>
2025-04-23 13:01:44 -0500

    added new methods to course

--- src/main/java/com/mycompany/app/Course.java
+++ src/main/java/com/mycompany/app/Course.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Course{
     private final String courseName;
@@ -50,8 +51,25 @@ public class Course{
     }
 
     //add assignment
+    public void gradeAssignment(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the grade for the following students: ");
+        for (int i = 0; i < classSize; i++){
+            System.out.print(roster.get(i).getFirstName() + " " + roster.get(i).getLastName() + ": ");
+            int grade = input.nextInt();
+            //add grade to gradebook
 
+            System.out.println();
+        }
+        input.close();
+    }
 
+    //add to gradebook for a specific student
+    public void addGrade(Student student, int grade){
+        //find student in gradebook and add grade
+    }
+
+    @Override
     public String toString(){
         String str = courseID + ": " +  courseName;
         str = str + "\nTeacher: " + teacher.getFirstName() + " " + teacher.getLastName();
