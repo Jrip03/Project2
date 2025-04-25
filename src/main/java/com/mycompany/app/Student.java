1a6dfaaf433855ae83cab3eaa25559f0b697cb0f
Ta0845 <AMBALATHANVI123@GMAIL.COM>
2025-04-24 23:17:46 -0500

    Final changes

--- src/main/java/com/mycompany/app/Student.java
+++ src/main/java/com/mycompany/app/Student.java
@@ -1,39 +1,70 @@
 package com.mycompany.app;
 
 public class Student {
-   private String studentID;
-   private String firstName;
-   private String lastName;
+     private String studentID;
+     private String firstName;
+     private String lastName;
    
-   //A Constructor for the student class
-   public Student(String fName, String lName, String ID) {
-     studentID = ID;
-     firstName = fName;
-     lastName = lName;
-   }
+     /**
+      * The Constructor for the student class
+      * 
+      *  @param fName student's first Name
+      *  @param lName student's last Name
+      *  @param ID student's ID
+      */
+     public Student(String fName, String lName, String ID) {
+      studentID = ID;
+      firstName = fName;
+      lastName = lName;
+     }
 
-   //Getter and setter for studentID
-   public String getStudentID() {
-    return studentID;
-   }
-   public void setStudentID(String studentID) {
-    this.studentID = studentID;
-   }
-
-   //Getter and setter for firstName
-   public String getFirstName() {
-    return firstName;
-   }
-   public void setFirstName(String firstName) {
-    this.firstName = firstName;
-   }
-
-   //Getter and setter for LastName
-   public String getLastName() {
-    return lastName;
-   }
-   public void setLastName(String lastName) {
-    this.lastName = lastName;
-   }
+     /**
+      *  Gets the student ID
+      * 
+      *  @return returns the ID of student
+      */
+     public String getStudentID() {
+      return studentID;
+     }
+     /**
+      *  Sets the student ID
+      * 
+      *  @return returns the ID of new student
+      */
+     public void setStudentID(String studentID) {
+      this.studentID = studentID;
+    }
+     /**
+      *  Gets the firstName of the student 
+      * 
+      *  @return returns the firstName of student
+      */
+     public String getFirstName() {
+      return firstName;
+     }
+     /**
+      *  Sets the firstName of the student
+      * 
+      *  @return returns the new firstName of student
+      */
+     public void setFirstName(String firstName) {
+      this.firstName = firstName;
+     }
+     /**
+      * Gets the lastName of the student
+      * 
+      *  @return returns the lastName of student
+      */
+     public String getLastName() {
+      return lastName;
+     }
+     /**
+      *  Sets the lastName of the student 
+      * 
+      *  @return returns the new lastName of student 
+      */
+     public void setLastName(String lastName) {
+      this.lastName = lastName;
+     }
 
 }
